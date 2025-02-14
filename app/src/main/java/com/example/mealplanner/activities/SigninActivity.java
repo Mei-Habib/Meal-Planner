525909cc5ed1ce86d2d5333fa6a0f152a28d14c6
Mei Habib <habibmai09@gmail.com>
2025-02-15 01:13:59 +0200

    added sign up screen ui

--- app/src/main/java/com/example/mealplanner/activities/SigninActivity.java
+++ app/src/main/java/com/example/mealplanner/activities/SigninActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.example.mealplanner.R;
 
 public class SigninActivity extends AppCompatActivity {
@@ -14,5 +18,10 @@ public class SigninActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
